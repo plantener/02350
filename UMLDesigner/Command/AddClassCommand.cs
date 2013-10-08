8624cf92740a9f72d7c1a33780b04a4f0a051a53
Jesper Plantener <jesper@plantener.dk>
2013-10-08 10:08:35 +0200

    Added command

---
+++ UMLDesigner/Command/AddClassCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace UMLDesigner.Command
+{
+    class AddClassCommand 
+    {
+        
+
+    }
+}
--- UMLDesigner/ViewModel/MainViewModel.cs
+++ UMLDesigner/ViewModel/MainViewModel.cs
@@ -1,11 +1,14 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using UMLDesigner.Command;
 using UMLDesigner.Model;
 
 namespace UMLDesigner.ViewModel
 {
  
+
     public class MainViewModel : ViewModelBase
     {
         public ObservableCollection<Class> Classes { get; set; }
@@ -17,14 +20,24 @@ namespace UMLDesigner.ViewModel
         public MainViewModel()
         {
 
+        AddClassCommand = new RelayCommand(AddClass);
+
+       
+
+
             // Her fyldes listen af classes med to classes. Her benyttes et alternativ til konstruktorer med syntaksen 'new Type(){ Attribut = Værdi }'
             // Det der sker er at der først laves et nyt object og så sættes objektets attributer til de givne værdier.
             Classes = new ObservableCollection<Class>()
             { 
                 new Class() { ClassName = "TestClass", X = 30, Y = 40, Attributes = {"Hej","Test"}, Methods = {"Vi","tester","mere"}, Properties={"properties"}},
                 new Class() { ClassName = "TestClass", X = 140, Y = 230, Methods = {"Endnu", "En", "test"}, Attributes = {"Attribut"}, Properties= {"properties", "her"}},
-                new Class() {ClassName = "NewClass", Attributes = {"Attributtest", "Attributtest2"}, Methods = { "MethodTest", "MethodTest2"}, Properties = {"PropertiesTest", "ProperTiesTest2"}}
+                new Class() { ClassName = "NewClass", Attributes = {"Attributtest", "Attributtest2"}, Methods = { "MethodTest", "MethodTest2"}, Properties = {"PropertiesTest", "ProperTiesTest2"}}
             };
+        }
+
+        public void AddClass()
+        {
+            Classes.Add(new Class() { ClassName = "AddedClass", X = 100, Y = 100 });
         }
     }
 }
