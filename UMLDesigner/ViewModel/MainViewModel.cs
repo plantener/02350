c4e9c5f4e61976e8ac6e3e75ac564ac0feae4b08
Jesper Plantener <jesper@plantener.dk>
2013-10-11 20:46:28 +0200

    Can now move classes around. Still something weird going on when moving though

--- UMLDesigner/ViewModel/MainViewModel.cs
+++ UMLDesigner/ViewModel/MainViewModel.cs
@@ -1,6 +1,7 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -38,13 +39,16 @@ namespace UMLDesigner.ViewModel
             AddClassCommand = new AddClassCommand(Classes);
             MouseDownNodeCommand = new RelayCommand<MouseButtonEventArgs>(MouseDownNode);
             MouseMoveNodeCommand = new RelayCommand<MouseEventArgs>(MouseMoveNode);
+            MouseUpNodeCommand = new RelayCommand<MouseButtonEventArgs>(MouseUpNode);
      
         }
 
         // Captures the mouse, to move nodes
         public void MouseDownNode(MouseButtonEventArgs e)
         {
-            e.MouseDevice.Target.CaptureMouse();
+           e.MouseDevice.Target.CaptureMouse();
+          
+           
         }
 
         //Used to move nodes around
@@ -53,9 +57,14 @@ namespace UMLDesigner.ViewModel
             //Is the mouse captured?
             if (Mouse.Captured != null)
             {
+
+                
                 FrameworkElement movingClass = (FrameworkElement)e.MouseDevice.Target;
+                
                 // Fra ellipsen skaffes punktet som den er bundet til.
                 Node movingNode = (Node)movingClass.DataContext;
+
+                
                 // Canvaset findes her udfra ellipsen.
                 Canvas canvas = FindParent<Canvas>(movingClass);
                 // Musens position i forhold til canvas skaffes her.
@@ -64,23 +73,61 @@ namespace UMLDesigner.ViewModel
                 // derfor gemmes her positionen før det første ryk så den sammen med den sidste position kan benyttes til at flytte punktet med en kommando.
                 if (moveNodePoint == default(Point)) moveNodePoint = mousePosition;
                 // Punktets position ændres og beskeden bliver så sendt til UI med INotifyPropertyChanged mønsteret.
+                
                 movingNode.X = (int)mousePosition.X;
                 movingNode.Y = (int)mousePosition.Y;
             }
         }
 
-        private T FindParent<T>(DependencyObject child)
-
-  where T : DependencyObject
+        public void MouseUpNode(MouseEventArgs e)
         {
+            //Used to move node
+            // noden skaffes.
+            FrameworkElement movingClass =(FrameworkElement) e.MouseDevice.Target;
+            
 
-            T parent = VisualTreeHelper.GetParent(child) as T;
+            // Ellipsens node skaffes.
+            Node movingNode = (Node) movingClass.DataContext;
+            // Canvaset skaffes.
+            Canvas canvas = FindParent<Canvas>(movingClass);
+            // Musens position på canvas skaffes.
+            Point mousePosition = Mouse.GetPosition(canvas);
+            // Punktet flyttes med kommando. Den flyttes egentlig bare det sidste stykke i en række af mange men da de originale punkt gemmes er der ikke noget problem med undo/redo.
+           new MoveNodeCommand(movingNode, (int)mousePosition.X, (int)mousePosition.Y, (int)moveNodePoint.X, (int)moveNodePoint.Y);
+            // Nulstil værdier.
+            moveNodePoint = new Point();
+            // Musen frigøres.
+            e.MouseDevice.Target.ReleaseMouseCapture(); 
+        }
 
+        public static T FindParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            //get parent item
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            //we've reached the end of the tree
+            if (parentObject == null) return null;
+
+            //check if the parent matches the type we're looking for
+            T parent = parentObject as T;
             if (parent != null)
+            {
                 return parent;
+            }
             else
-                return FindParent<T>(parent);
-
+            {
+                return FindParent<T>(parentObject);
+            }
+        }
+        //Currently not used
+        private Node GetNodeUnderMouse()
+        {
+            var item = Mouse.DirectlyOver as DockPanel;
+            if (item == null)
+            {
+                return null;
+            }
+            return item.DataContext as Node;
         }
     }
 }
