a06818b7a411619a15b50a87b2f2017b34f1c3fa
ttp <ttp@it-minds.dk>
2013-11-05 11:03:47 +0100

    Added undo/redo keyboard shortcuts
    And changed undo/redo function in UndoRedoController to work better.

--- UMLDesigner/View/MainWindow.xaml.cs
+++ UMLDesigner/View/MainWindow.xaml.cs
@@ -12,17 +12,45 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GalaSoft.MvvmLight.Command;
+using UMLDesigner.Command;
 
-namespace UMLDesigner
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+namespace UMLDesigner {
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window {
+      public MainWindow() {
+         InitializeComponent();
+      }
+
+      private bool _pressed = false;
+      private void Window_KeyDown(object sender, KeyEventArgs e) {
+         if (Keyboard.Modifiers == ModifierKeys.Control && _pressed == false) {
+            if (Keyboard.IsKeyDown(Key.Z)) {
+               UndoRedoController.GetInstance().Undo();
+               _pressed = true;
+            }
+            if (Keyboard.IsKeyDown(Key.Y)) {
+               UndoRedoController.GetInstance().Redo();
+               _pressed = true;
+            }
+
+
+         }
+
+         e.Handled = true;
+      }
+
+      private void Window_KeyUp(object sender, KeyEventArgs e) {
+         if (_pressed == true) {
+            System.Diagnostics.Debug.WriteLine("unpressed");
+            _pressed = false;
+         }
+
+      }
+
+
+
+   }
 }
