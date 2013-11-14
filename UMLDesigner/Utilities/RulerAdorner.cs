b0299bdb0fcf985a9a98149c1ef29d7f700af3a5
Jesper Plantener <jesper@plantener.dk>
2013-11-14 19:51:04 +0100

    Added grid overlay, still cannot use the checkbox

---
+++ UMLDesigner/Utilities/RulerAdorner.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace UMLDesigner.Utilities
+{
+    public class RulerAdorner : Adorner
+    {
+        private FrameworkElement element;
+        public RulerAdorner(UIElement el)
+            : base(el)
+        {
+            element = el as FrameworkElement;
+        }
+
+        protected override void OnRender(System.Windows.Media.DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            double height = element.ActualHeight;
+            double width = element.ActualWidth;
+
+            double linesHorizontal = height / 50;
+            double linesVertical = width / 50;
+
+            var pen = new Pen(Brushes.LightGray, 1) { StartLineCap = PenLineCap.Triangle, EndLineCap = PenLineCap.Triangle };
+
+            int offset = 0;
+
+            for (int i = 0; i <= linesVertical; ++i)
+            {
+                offset = offset + 50;
+                drawingContext.DrawLine(pen, new Point(offset, 0), new Point(offset, height));
+            }
+
+            offset = 0;
+
+            for (int i = 0; i <= linesHorizontal; ++i)
+            {
+                offset = offset + 50;
+                drawingContext.DrawLine(pen, new Point(0, offset), new Point(width, offset));
+            }
+        }
+    }
+}
---
+++ UMLDesigner/View/GridWithRulerxaml.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using UMLDesigner.Utilities;
+
+namespace UMLDesigner.View
+{
+    /// <summary>
+    /// Interaction logic for GridWithRulerxaml.xaml
+    /// </summary>
+    public partial class GridWithRulerxaml : UserControl
+    {
+        public GridWithRulerxaml()
+        {
+            InitializeComponent();
+
+            //Loaded event is necessary as Adorner is null until control is shown.
+            Loaded += GridWithRulerxaml_Loaded;
+
+        }
+
+        void GridWithRulerxaml_Loaded(object sender, RoutedEventArgs e)
+        {
+            var adornerLayer = AdornerLayer.GetAdornerLayer(this);
+            var rulerAdorner = new RulerAdorner(this);
+            adornerLayer.Add(rulerAdorner);
+        }
+    }
+}
