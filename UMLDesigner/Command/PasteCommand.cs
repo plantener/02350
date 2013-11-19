a31efb2b20cc2c46fce72298fb8ba1a21fb0c559
Carsten Jacobsen <callefix91@gmail.com>
2013-11-19 09:32:33 +0100

    Paste almost done

---
+++ UMLDesigner/Command/PasteCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UMLDesigner.ViewModel;
+
+namespace UMLDesigner.Command
+{
+    class PasteCommand : IUndoRedoCommand
+    {
+        private ObservableCollection<NodeViewModel> _classes;
+        private NodeViewModel _copyClass = null;
+
+        public PasteCommand(ObservableCollection<NodeViewModel> classes, NodeViewModel copyClass)
+        {
+            _classes = classes;
+            _copyClass = copyClass;
+        }
+
+        public void Execute()
+        {
+            _classes.Add(_copyClass);
+        }
+
+        public void UnExecute()
+        {
+            _classes.Remove(_copyClass);
+        }
+    }
+}
