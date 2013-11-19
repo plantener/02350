60fad6449aa51bb9746892b50704c9e53fbfc9b5
ttp <ttp@it-minds.dk>
2013-11-19 10:31:33 +0100

    Added ClassUserControl InitializeComponent();

--- UMLDesigner/View/ClassUserControl.xaml.cs
+++ UMLDesigner/View/ClassUserControl.xaml.cs
@@ -16,45 +16,14 @@ using System.Windows.Shapes;
 
 namespace UMLDesigner.View
 {
-    /// <summary>
-    /// Interaction logic for ClassUserControl.xaml
-    /// </summary>
-    public partial class ClassUserControl : UserControl
-    {
-        ObservableCollection<GameData> _GameCollection =
-        new ObservableCollection<GameData>();
-
-        public ClassUserControl()
-        {
-            _GameCollection.Add(new GameData
-            {
-                GameName = "World Of Warcraft",
-                Creator = "Blizzard",
-                Publisher = "Blizzard"
-            });
-            _GameCollection.Add(new GameData
-            {
-                GameName = "Halo",
-                Creator = "Bungie",
-                Publisher = "Microsoft"
-            });
-            _GameCollection.Add(new GameData
-            {
-                GameName = "Gears Of War",
-                Creator = "Epic",
-                Publisher = "Microsoft"
-            });
-            InitializeComponent();
-        }
-
-        public ObservableCollection<GameData> GameCollection
-        { get { return _GameCollection; } }
-
-        public class GameData
-        {
-            public string GameName { get; set; }
-            public string Creator { get; set; }
-            public string Publisher { get; set; }
-        }
-    }
+   /// <summary>
+   /// Interaction logic for ClassUserControl.xaml
+   /// </summary>
+   public partial class ClassUserControl : UserControl
+   {
+      public ClassUserControl()
+      {
+         InitializeComponent();
+      }
+   }
 }
