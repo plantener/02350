c87dfd1f4ee1e3c4dd73957ef361ecb2e214efc3
Carsten Jacobsen <callefix91@gmail.com>
2013-11-26 10:21:41 +0100

    Alles funktionirt

--- UMLDesigner/ViewModel/MainViewModel.cs
+++ UMLDesigner/ViewModel/MainViewModel.cs
@@ -22,14 +22,14 @@ namespace UMLDesigner.ViewModel {
       private int relativeMousePositionX = -1;
       private int relativeMousePositionY = -1;
       private bool deleteActive = false;
-      public bool DeleteActive { get { return deleteActive; } set { if (IsFocused || EdgeIsFocused) { deleteActive = true; } else { deleteActive = false;  RaisePropertyChanged(() => DeleteActive); } } }
+      public bool DeleteActive { get { if (isFocused || edgeIsFocused) { deleteActive = true; return deleteActive; } else { deleteActive = false; return deleteActive; } } set { deleteActive = value; RaisePropertyChanged(() => DeleteActive); } }
       private bool isFocused = false;
-      public bool IsFocused { get { return isFocused; } set { isFocused = value; RaisePropertyChanged(() => IsFocused); } }
+      public bool IsFocused { get { return isFocused; } set { isFocused = value; RaisePropertyChanged(() => IsFocused); RaisePropertyChanged(() => DeleteActive); } }
       private NodeViewModel focusedClass = null;
-      public NodeViewModel FocusedClass { get { return focusedClass; } private set { focusedClass = value; if (focusedClass == null) { IsFocused = false; } else { IsFocused = true; FocusedEdge = null; };} }
+      public NodeViewModel FocusedClass { get { return focusedClass; } private set { focusedClass = value; if (focusedClass == null) { IsFocused = false;  } else { IsFocused = true; FocusedEdge = null; };} }
       //Used for detecting focus on edges
       private bool edgeIsFocused = false;
-      public bool EdgeIsFocused { get { return edgeIsFocused; } set { edgeIsFocused = value; RaisePropertyChanged(() => EdgeIsFocused); } }
+      public bool EdgeIsFocused { get { return edgeIsFocused; } set { edgeIsFocused = value; RaisePropertyChanged(() => EdgeIsFocused); RaisePropertyChanged(() => DeleteActive); } }
       private EdgeViewModel focusedEdge = null;
       public EdgeViewModel FocusedEdge { get { return focusedEdge; } private set { focusedEdge = value; if (focusedEdge == null) { EdgeIsFocused = false; } else { EdgeIsFocused = true; FocusedClass=null; };} }
       private bool canPaste = false;
@@ -136,6 +136,7 @@ namespace UMLDesigner.ViewModel {
           if (FocusedClass != null)
           {
               undoRedoController.AddAndExecute(new DeleteClassCommand(Classes, FocusedClass, Edges));
+              FocusedClass = null;
           }
           else if (FocusedEdge != null)
           {
